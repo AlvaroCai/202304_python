1a4306963c31a5ffe02cfd223882613fef4feeb2
Alvaro Caicedo <aacaicedorodriguez@gmail.com>
2023-06-24 19:08:20 -0400

    Hola_Mundo

---
+++ Holamundo.py
@@ -0,0 +1,13 @@
+# 1. TAREA: imprime "Hola, mundo"
+print("Hola Mundo")
+# 2. imprime "Hola, Noelle" con el nombre en una variable
+name = "Noelle"
+print( "hola,"+ name )	# con una coma
+# 3. imprimir "Hola 42!" con el número en una variable
+name = 42
+print( "Hola " + str(name) + " ! ")	# con un +
+# 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "Amo comer " + fave_food1+ " y " + fave_food2 + "con las comidas")	
+
